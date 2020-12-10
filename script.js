ff358a06ff0a1729e0df6530ebc4b2c56caee7b0
Prince Csaba <bolyaicsaba@gmail.com>
2020-12-10 09:30:14 +0100

    project scope and teoretic steps made

---
+++ script.js
@@ -0,0 +1,30 @@
+// todo appot készítünk, alapértelmezésben 10 üres mező áll rendelkezésre,
+// de hozzá tudunk adni újakat, minden mező előtt van 1 csekkbox, textimput mező,
+// van egy törlés gomb, mivel ki tudjuk törölni a sort, ha nincs rá szükség
+
+function(pageLoad) {
+  root = document.getElementById("root");
+  
+  //egy változóba eltárolni egy max értéket, amivel dolgozunk
+  
+  //elmenteni egy sort a változóba: div, checkbox, 1 text input, 1 button
+  
+  //lesz egy for amivel hozzáadjuk a roothoz
+  
+  //kell egy gomb, amivel hozzáadunk a listán kívükre egy gombot - insertadjest: afterend
+  
+  
+  //klikk esemény a gombhoz, a változóban elárolt elemet hozzáadjuk a változóhoz
+  
+  
+  //classnév alapján kijelölöm az összes törlés gombot és elmentem egy változóba
+  
+
+  //for ciklus - ráadni egy klikk eseményt mindegyikre
+  //hogyan jelölöm ki az elemet, amit ki szeretnék törölni?
+
+  //
+
+}
+
+window.addEventListener("load", pageLoad);
